5da32720fd1d65821d890f2afe471c8d06eaaaf5
Clark <nttung1211@gmail.com>
2019-10-12 16:31:37 +0700

    Completed automatic sprite generation.

---
+++ gulp/tasks/sprite.js
@@ -0,0 +1,39 @@
+let gulp = require(`gulp`),
+    rename = require(`gulp-rename`),
+    svgSprite = require(`gulp-svg-sprite`),
+    del = require(`del`),
+    config = {
+        mode: {
+            css: {
+                sprite: `sprite.svg`,
+                render: {
+                    css:{
+                        template:`./gulp/templates/sprite.css`
+                    }
+                }
+            }
+        }
+    };
+
+gulp.task(`beginClean`, () => 
+    del([`./app/temp/sprite`, `./app/assets/images/sprite`])
+);
+
+gulp.task(`createSprite`, [`beginClean`], () => 
+    gulp.src(`./app/assets/images/icons/**/*.svg`)
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest(`./app/temp/sprite/`))
+);
+
+gulp.task(`copySpriteGraphic`, [`createSprite`], () => 
+    gulp.src(`./app/temp/sprite/css/**/*.svg`)
+    .pipe(gulp.dest(`./app/assets/images/sprite`))
+);
+
+gulp.task(`copySprite`, [`createSprite`], () => 
+    gulp.src(`./app/temp/sprite/css/*.css`)
+    .pipe(rename(`_sprite.css`))
+    .pipe(gulp.dest(`./app/assets/css/modules`))
+);
+
+gulp.task(`icon`, [`beginClean`, `createSprite`, `copySpriteGraphic`, `copySprite`]);
